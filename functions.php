c53c52594a02d26ef4676ca61e173056c02a3045
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-09-28 12:45:51 +0300

    functions demo

--- functions.php
+++ functions.php
@@ -1,6 +1,16 @@
 <?php
     //functions.php
     //siia tulevad funktsioonid
+    function hello1 ($name, $age){
+        echo "Tere ".$name." kes on ".$age." aastat vana";
+    }
     
+    function hello2 ($name){
+        return "Tere ".$name;
+    }
     
+    echo "<br>";
+    hello1("Juku", 5);
+    echo "<br>";
+    echo hello2("Juhan");
 ?>
